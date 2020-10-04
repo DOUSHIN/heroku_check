c21f6798f1fcf9cbc9d8dcfc181be72154398657
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-10-04 17:32:58 +0900

    「お気に入り機能」実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :logins, only: [:new, :create, :destroy]
+  resources :favorites, only: [:create, :destroy]
   resources :pictures do
     collection do
       post :confirm
