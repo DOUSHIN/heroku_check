7e4905c1334d0b4c8f497df87107ac9f9c16ac50
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-10-04 15:23:28 +0900

    「プロフィール編集機能」実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.now[:change] = "アカウント詳細変更完了"
+      render :show
+    else
+      flash.now[:notgood] = "エラー"
+      render :new
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :image, :image_cache, :password, :password_confirmation)
