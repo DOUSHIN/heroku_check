42aec9105069d7c4f3ceb1f017682117b47b6138
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-10-10 15:13:12 +0900

    本番環境でメールを送る

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: @picture.user.email, subject: "投稿確認メール"
+    mail to: "hiroyuki.nakagawa0325@gmail.com", subject: "投稿確認メール"
   end
 end
