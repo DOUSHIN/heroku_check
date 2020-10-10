9b1fb5e0033acb4a319d137ca507b80fa546a1d9
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-10-11 07:56:42 +0900

    letter_opener_webを本番環境でも利用

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   root 'users#new'
 end
