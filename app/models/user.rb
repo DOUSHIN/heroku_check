8aef1136ad1b8adf43f72f4bdf422b3ad59c5336
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-10-12 12:21:20 +0900

    Userのプロフィール部分を編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum:6}
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :image, presence: true
 end
